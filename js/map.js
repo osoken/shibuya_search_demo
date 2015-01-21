dc2df19eadb82f0018b8fe824aa51010e1e2ad99
itagaki <itagaki.shintaro@gmail.com>
2015-01-21 17:00:11 +0900

    中２階に対応

--- js/map.js
+++ js/map.js
@@ -4,6 +4,7 @@ var MAX_LAT = 139.702,
     MIN_LAT = 139.699;
 var MAX_LNG = 35.6589,
     MIN_LNG = 35.6584;
+var FLOOR_LEVEL = [1, 105, 2, 3]; 
 
 d3.csv("data/Shibuya_Point.csv", function(error, data) {
     if (error != null) {
@@ -12,25 +13,25 @@ d3.csv("data/Shibuya_Point.csv", function(error, data) {
     }
     var floor_data = [];
     // TODO: TODO: 1〜3階固定ではなく、動的にする。
-    for (var i = 1; i <= 3; i++) {
+    FLOOR_LEVEL.forEach(function(floor_level){
         // 階ごとにデータを生成
-        floor_data[i] = data.filter(function(element) {
-            return element.floorLevel === String(i);
+        floor_data[floor_level] = data.filter(function(element) {
+            return element.floorLevel === String(floor_level);
         });
         // svgタグを生成
-        var svg = d3.select("#floor_" + i)
+        var svg = d3.select("#floor_" + floor_level)
             .append("svg")
             .attr("width", "100%")
             .attr("height", "100%");
         // ポイントごとに円を生成
         svg.selectAll("circle")
-            .data(floor_data[i])
+            .data(floor_data[floor_level])
             .enter().append("circle")
             .attr("cx", function(d) {
-                return (d.lat - MIN_LAT) / (MAX_LAT - MIN_LAT) * $("#floor_" + i).width();
+                return (d.lat - MIN_LAT) / (MAX_LAT - MIN_LAT) * $("#floor_" + floor_level).width();
             })
             .attr("cy", function(d) {
-                return (MAX_LNG - d.lng) / (MAX_LNG - MIN_LNG) * $("#floor_" + i).height();
+                return (MAX_LNG - d.lng) / (MAX_LNG - MIN_LNG) * $("#floor_" + floor_level).height();
             })
             .attr("r", 10)
             .attr("fill", "blue")
@@ -41,13 +42,13 @@ d3.csv("data/Shibuya_Point.csv", function(error, data) {
             });
         // ポイントごとに名前を生成
         svg.selectAll("text")
-            .data(floor_data[i])
+            .data(floor_data[floor_level])
             .enter().append("text")
             .attr("x", function(d) {
-                return (d.lat - MIN_LAT) / (MAX_LAT - MIN_LAT) * $("#floor_" + i).width() + 10;
+                return (d.lat - MIN_LAT) / (MAX_LAT - MIN_LAT) * $("#floor_" + floor_level).width() + 10;
             })
             .attr("y", function(d) {
-                return (MAX_LNG - d.lng) / (MAX_LNG - MIN_LNG) * $("#floor_" + i).height();
+                return (MAX_LNG - d.lng) / (MAX_LNG - MIN_LNG) * $("#floor_" + floor_level).height();
             })
             .text(function(d) {
                 return d.Name;
@@ -55,5 +56,5 @@ d3.csv("data/Shibuya_Point.csv", function(error, data) {
             .attr({
                 'dominant-baseline': 'middle'
             });
-    }
+    });
 });
